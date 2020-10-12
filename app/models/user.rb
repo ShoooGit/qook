2b55dcf5abd42342ac2b6a1f2d51fdd103f3091d
ShoooGit <shooo.ity.777@gmail.com>
2020-10-12 17:59:15 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # アソシエーションの設定
+  # has_many :recipes
+  # has_many :refrigerators
+
+  # バリデーションの設定
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Input half-width characters.' }
+
 end
