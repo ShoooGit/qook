56c9eaec9e92dd6f1d5d568292a7ca023e90e03b
ShoooGit <shooo.ity.777@gmail.com>
2020-10-15 15:20:24 +0900

    レシピモデルのバリデーション・アソシエーション設定

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,16 @@
 class Recipe < ApplicationRecord
+  # アソシエーションの設定
+  belongs_to :user
+  has_many :recipe_ingredients
+  has_one_attached :image
+
+  # バリデーションの設定
+  validates :name, presence: true
+  validates :image, presence: true
+  with_options numericality: {
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: 9_999
+  } do
+    validates :calorie, :time
+  end
 end
