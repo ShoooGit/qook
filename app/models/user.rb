8000f18df1dd03b8090ff27695f59c4e0559514f
ShoooGit <shooo.ity.777@gmail.com>
2020-10-15 14:51:26 +0900

    ユーザーモデルのアソシエーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # アソシエーションの設定
-  # has_many :recipes
+  has_many :recipes
   # has_many :refrigerators
 
   # バリデーションの設定
