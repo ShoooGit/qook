caa60892b21e7d598379cda206318ebcfa490b13
ShoooGit <shooo.ity.777@gmail.com>
2020-10-21 16:46:23 +0900

    アクセス制限の設定

--- app/controllers/refrigerators_controller.rb
+++ app/controllers/refrigerators_controller.rb
@@ -1,5 +1,8 @@
 class RefrigeratorsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_refrigerator, only: [:edit, :update]
+  before_action :move_to_top_new, only: :new
+  before_action :move_to_top_edit, only: :edit
 
   def new
     @refrigerator = Refrigerator.new
@@ -35,5 +38,15 @@ class RefrigeratorsController < ApplicationController
 
   def set_refrigerator
     @refrigerator = Refrigerator.find(params[:id])
+  end
+
+  def move_to_top_new
+    # すでに冷蔵庫の存在するユーザが冷蔵庫作成ページにアクセスした場合、トップページにリダイレクト
+    redirect_to root_path if current_user.refrigerator
+  end
+
+  def move_to_top_edit
+    # 別ユーザの冷蔵庫にアクセスした場合、トップページにリダイレクト
+    redirect_to root_path if current_user.id != @refrigerator.user_id
   end
 end
