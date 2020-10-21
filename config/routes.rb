e7a2df3c624b70ceab8c25c03cbb834169a66d69
ShoooGit <shooo.ity.777@gmail.com>
2020-10-21 16:27:28 +0900

    ルーティングの設定&微修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'recipes#index'
   devise_for :users
   resources :recipes
-  resources :refrigerators, only: [:new, :create]
+  resources :refrigerators, only: [:new, :create, :edit, :update]
 end
