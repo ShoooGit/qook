dbe4497b953a29cb3989429e42a05bfa2690c850
ShoooGit <shooo.ity.777@gmail.com>
2020-10-21 12:03:56 +0900

    マイグレーションの実行

--- db/migrate/20201021030214_create_refrigerators.rb
+++ db/migrate/20201021030214_create_refrigerators.rb
@@ -1,7 +1,7 @@
 class CreateRefrigerators < ActiveRecord::Migration[6.0]
   def change
     create_table :refrigerators do |t|
-
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_114420) do
+ActiveRecord::Schema.define(version: 2020_10_21_030214) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -52,6 +52,13 @@ ActiveRecord::Schema.define(version: 2020_10_15_114420) do
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
+  create_table "refrigerators", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_refrigerators_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "nickname", limit: 255, null: false
     t.string "email", limit: 255, default: "", null: false
@@ -68,4 +75,5 @@ ActiveRecord::Schema.define(version: 2020_10_15_114420) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "recipe_ingredients", "recipes"
   add_foreign_key "recipes", "users"
+  add_foreign_key "refrigerators", "users"
 end
