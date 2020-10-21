01a7c05aab332907fc0be9c78598ffed64e1618e
ShoooGit <shooo.ity.777@gmail.com>
2020-10-21 16:33:37 +0900

    edit,updateアクションの追加

--- app/controllers/refrigerators_controller.rb
+++ app/controllers/refrigerators_controller.rb
@@ -1,4 +1,6 @@
 class RefrigeratorsController < ApplicationController
+  before_action :set_refrigerator, only: [:edit, :update]
+
   def new
     @refrigerator = Refrigerator.new
   end
@@ -12,11 +14,26 @@ class RefrigeratorsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @refrigerator.update(refrigerator_params)
+      redirect_to root_path
+    else
+      render action: :edit
+    end
+  end
+
   private
 
   def refrigerator_params
     params.require(:refrigerator)
           .permit(refrigerator_ingredients_attributes: [:id, :ingredient_id, :quantity, :_destroy])
           .merge(user_id: current_user.id)
+  end
+
+  def set_refrigerator
+    @refrigerator = Refrigerator.find(params[:id])
   end
 end
