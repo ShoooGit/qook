2276c6f081b0e57e1406fef8b560621fab050b5e
ShoooGit <shooo.ity.777@gmail.com>
2020-10-25 20:58:50 +0900

    レシピ登録の結合テスト

--- spec/system/recipes_spec.rb
+++ spec/system/recipes_spec.rb
@@ -2,27 +2,42 @@ require 'rails_helper'
 
 RSpec.describe 'レシピ登録', type: :system do
   before do
-    @user = FactoryBot.create(:user)
-    @tweet_text = Faker::Lorem.sentence
-    @tweet_image = Faker::Lorem.sentence
+    @recipe_ingredient = FactoryBot.build(:recipe_ingredient)
+    @recipe = @recipe_ingredient.recipe
+    @user = @recipe.user
+    @user.save
   end
   context 'レシピ登録ができるとき'do
     it 'ログインしたユーザーはレシピ登録ができる' do
       # ログインする
       sign_in(@user)
-      # 新規投稿ページへのリンクがあることを確認する
-      # 投稿ページに移動する
+      # レシピ登録ページへのリンクがあることを確認する
+      expect(page).to have_content('レシピ登録')
+      # レシピ登録ページに移動する
+      find('.add-recipe').click
       # フォームに情報を入力する
-      # 送信するとTweetモデルのカウントが1上がることを確認する
-      # 投稿完了ページに遷移することを確認する
-      # 「投稿が完了しました」の文字があることを確認する
-      # トップページに遷移する
-      # トップページには先ほど投稿した内容のツイートが存在することを確認する（画像）
+      fill_in 'recipe-name', with: @recipe.name
+      fill_in 'calorie', with: @recipe.calorie
+      fill_in 'time', with: @recipe.time
+      image_path = File.join(Rails.root, "public/images/test.jpg")
+      page.('.image').set(image_path)
+      find('option[value="14"]').select_option
+      fill_in 'quantity', with: @recipe_ingredient.quantity
+      # 登録するとRecipeモデルとRecipeIngredientモデルのカウントが1上がることを確認する
+      expect  do
+        find('input[name="commit"]').click
+      end.to change { Recipe.count }.by(1).and change { RecipeIngredient.count }.by(1)
+      # レシピ登録後は、トップページに遷移することを確認する
+      expect(current_path).to eq root_path
+      # 「カレーライスのレシピを登録しました」の文字があることを確認する
+      expect(page).to have_content("#{@recipe.name}のレシピを登録しました")
+      # トップページには先ほど登録したレシピが存在することを確認する（画像）
+      expect(page).to have_selector("img[src$=#{@recipe.image}]")
       # トップページには先ほど投稿した内容のツイートが存在することを確認する（テキスト）
     end
   end
   context 'ツイート投稿ができないとき'do
-    it 'ログインしていないとレシピ登録ページに遷移できない' do
+    it 'ログインしていないとトップページとレシピ登録ページに遷移できない' do
       # トップページに遷移する
       # 新規投稿ページへのリンクがない
     end
