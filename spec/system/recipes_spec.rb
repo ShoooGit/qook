d661460e4b819c314f011a762a0dd44ebf161b54
ShoooGit <shooo.ity.777@gmail.com>
2020-10-26 16:21:51 +0900

    fakerのインストール

--- spec/system/recipes_spec.rb
+++ spec/system/recipes_spec.rb
@@ -33,6 +33,40 @@ RSpec.describe 'レシピ登録', type: :system do
       # トップページには先ほど登録したレシピが存在することを確認する（画像）
       expect(page).to have_selector("img[src$='test.jpg']")
       # トップページには先ほど投稿した内容のツイートが存在することを確認する（テキスト）
+      expect(page).to have_content("#{@recipe.name}")
+    end
+  end
+  context 'レシピ登録ができないとき'do
+    it 'ログインしていないとトップページとレシピ登録ページに遷移できない' do
+      # レシピ登録ページに移動する
+      visit new_recipe_path
+      # ログインページに遷移させられる
+      expect(current_path).to eq new_user_session_path
+    end
+  end
+end
+
+RSpec.describe 'レシピ編集', type: :system do
+  before do
+    @recipe1 = FactoryBot.create(:recipe)
+    @recipe2 = FactoryBot.create(:recipe)
+  end
+  context 'レシピ編集ができるとき'do
+    it 'ログインしたユーザーは自分が登録したレシピの編集ができる' do
+      # レシピ1を登録したユーザーでログインする
+      sign_in(@recipe1.user)
+      # レシピ1のリンクをクリックする
+      click_link "/recipes/#{@recipe1.id}"
+      # 編集ページへ遷移する
+
+      # すでに投稿済みの内容がフォームに入っていることを確認する
+      # 投稿内容を編集する
+      # 編集してもTweetモデルのカウントは変わらないことを確認する
+      # 編集完了画面に遷移したことを確認する
+      # 「更新が完了しました」の文字があることを確認する
+      # トップページに遷移する
+      # トップページには先ほど変更した内容のツイートが存在することを確認する（画像）
+      # トップページには先ほど変更した内容のツイートが存在することを確認する（テキスト）
     end
   end
   context 'レシピ登録ができないとき'do
