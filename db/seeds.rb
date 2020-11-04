d33a1871b711c97c05abd34924fb936ce027dec3
ShoooGit <shooo.ity.777@gmail.com>
2020-11-04 21:49:23 +0900

    画像保存の仕組み作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,13 @@
 # ゲストユーザーの作成
 User.create(id: 1, nickname: "ゲスト", email: "guest@com", password: "guestpass1")
+
+recipe = Recipe.new(
+  user_id: 1,
+  name: 'test',
+  calorie: 1000,
+  time: 60
+)
+
+recipe.image.attach(io: File.open('public/images/test.jpg'), filename: 'test.jpg')
+
+recipe.save
